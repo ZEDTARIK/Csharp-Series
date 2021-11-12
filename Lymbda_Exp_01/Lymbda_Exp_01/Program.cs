32b660cf2da0c1cbc9891b139ab30f1189b70aa1
ETTARAK <ettarak.zouhair@gmail.com>
2021-11-12 16:40:40 +0100

    Lymbda Syntaxe in CSharp

---
+++ Lymbda_Exp_01/Lymbda_Exp_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lymbda_Exp_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" \n \n ####################  Lymbda  #################### ");
+
+            // Declare array list type string 
+
+            List<string> Colors = new List<string> { "Red", "Bleu", "Orange", "Yellow" };
+
+            Console.WriteLine("The colors are : ");
+
+            Colors.ForEach(item =>
+            {
+                Console.WriteLine("\t - " + item);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
