33585b8d5c069173251ca704ffb218daa2cca2bd
ETTARAK <ettarak.zouhair@gmail.com>
2021-11-12 17:10:32 +0100

    Syntaxe for(int i=1;...; i++) ,Multiplication Table

---
+++ BoucleFor/BoucleFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BoucleFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n  #####################  Multiplication Table ####################### \n ");
+
+            for(int i = 1; i <= 10; i++) 
+            {
+                for(int j= 1; j <= 10; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + (i * j));
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
