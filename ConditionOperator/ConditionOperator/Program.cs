9eedf64fd6d525d31f9cce3f3901160746a1acd6
ETTARAK <ettarak.zouhair@gmail.com>
2021-11-12 16:58:58 +0100

    Condition Operator

---
+++ ConditionOperator/ConditionOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConditionOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("############################ Condition Operator ###############################");
+            
+            int Number1, Number2;
+            string msg;
+
+            Console.Write("Please Enter the First Number 1 =  " );
+            Number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter the First Number 2 =  ");
+            Number2 = int.Parse(Console.ReadLine());
+
+            // Result Division 
+
+               msg = Number2 == 0 ? "\n Warining, Cannot divide by Zero Please try again !!" : "The Result is = " + Number1 / Number2;
+
+            Console.Write(msg);
+
+            Console.ReadKey();
+        }
+    }
+}
